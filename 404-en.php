43e82421a759e081d03c3487723db343a7afdd69
Kristýna Adlofová <kristyna.adlofova@rvlt.digital>
2025-05-04 12:57:31 +0200

    prices changed

---
+++ 404-en.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Fri May 12 2023 10:46:13 GMT+0000 (Coordinated Universal Time)  -->
+<html lang="en" data-wf-page="6408805ede89a24399551718" data-wf-site="63e4c1b68c122b237942add3">
+<head>
+  <meta charset="utf-8">
+  <meta name="robots" content="noindex"/>
+  <title>Page not found | Gaudeo Guesthouses</title>
+  <meta content="Page not found" property="og:title">
+  <meta content="https://penzionygaudeo.cz/images/640f38838c27926c1ed5f931_9S5A2846.jpg" property="og:image">
+  <meta content="Page not found" property="twitter:title">
+  <meta content="https://penzionygaudeo.cz/images/640f38838c27926c1ed5f931_9S5A2846.jpg" property="twitter:image">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="/css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="/css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="/css/gaudeo.webflow.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"]  }});</script>
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="/images/favicon.jpg" rel="shortcut icon" type="image/x-icon">
+  <link href="/images/webclip.jpg" rel="apple-touch-icon">
+  <script type="text/javascript" charset="UTF-8" src="https://cdn.cookie-script.com/s/ec5f827c637b11ecfaf78703d47478df.js"></script>
+  <!--  Google tag (gtag.js)  -->
+  <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-Q3HG813LHS"></script>
+  <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-Q3HG813LHS'); </script>
+</head>
+<body>
+  <div class="header-container kontaktujte-n-s">
+    <div data-collapse="medium" data-animation="over-left" data-duration="400" fs-scrolldisable-element="smart-nav" data-easing="ease" data-easing2="ease" role="banner" class="navbar3_component w-nav">
+      <div class="navbar3_container">
+        <div id="w-node-_18b5c77c-a4d1-8ede-b754-d4ecf4160b02-48fc6785" class="white-nav-contact">
+          <a href="tel:+420774869840" class="text white-nav-contact"><span class="nav-phone">+420 774 869 840 </span><span class="no-wrap _12px">(Mo–Su 9:00-16:00)</span></a>
+          <a href="mailto:recepce@penzionygaudeo.cz?subject=P%C5%99i%C5%A1el%20V%C3%A1m%20email%20z%20webu%20Gaudeo" class="text no-underline"><strong class="text white-nav-text-email">recepce@penzionygaudeo.cz</strong></a>
+        </div>
+        <div id="w-node-_539c8af7-c7f7-54d1-88dc-e53badc99d5f-48fc6785" class="lang">
+          <a id="w-node-ff50ab8b-b072-5bc1-0330-b9f1d1023e1b-48fc6785" href="../404" class="lang-cs">CZ</a>
+          <div class="devider-lang"></div>
+        </div>
+      </div>
+    </div>
+    <div data-animation="default" class="navbar1_component-2 w-nav" data-easing2="ease" fs-scrolldisable-element="smart-nav" data-easing="ease" data-collapse="medium" data-w-id="f6747e0b-e634-f0f6-8ac6-6fb23cb4e587" role="banner" data-duration="400">
+      <div class="navbar1_container-2">
+        <a href="../en/" class="navbar1_logo-link w-nav-brand"><img src="../images/GAUDEO-LOGO-WHITE.svg" loading="lazy" width="63" alt="logo Gaudeo v bílé barvě" class="navbar1_logo-2"></a>
+        <nav role="navigation" class="navbar1_menu is-page-height-tablet w-nav-menu">
+          <a href="../en/accomodation" class="navbar1_link-2 w-nav-link">ACCOMMODATION</a>
+          <a href="../en/restaurant" class="navbar1_link-2 w-nav-link">RESTAURANT</a>
+          <a href="../en/private-events" class="navbar1_link-2 w-nav-link">PRIVATE EVENTS</a>
+          <a href="../en/about" class="navbar1_link-2 w-nav-link">ABOUT</a>
+          <a href="../en/contact" class="navbar1_link-2 w-nav-link">CONTACT</a>
+          <div class="devider-hamburger"></div>
+          <a href="../404" class="lang-en lang-hamburger">CZ</a>
+          <div class="navbar1_menu-buttons hamburger-mt-0">
+            <a href="reservation" class="button btn-hlavicka btn-hamburger w-button">RESERVATION</a>
+          </div>
+        </nav>
+        <div class="navbar1_menu-button w-nav-button">
+          <div class="menu-icon1">
+            <div class="menu-icon1_line-top"></div>
+            <div class="menu-icon1_line-middle">
+              <div class="menu-icon_line-middle-inner"></div>
+            </div>
+            <div class="menu-icon1_line-bottom"></div>
+          </div>
+        </div>
+      </div>
+      <div class="gradient-down"></div>
+    </div>
+  </div>
+  <section data-w-id="8adc0458-1865-7b9e-053d-7bdeb3e45202" class="section_cta57">
+    <div class="padding-global bg-gray">
+      <div class="container-small">
+        <div class="padding-section-large mt-2rem">
+          <div class="text-align-center-3">
+            <div class="margin-bottom margin-small">
+              <div class="cta57_heading-top">
+                <h2 class="h1 black _404-headline _404-m">Unfortunately, the page you</h2>
+              </div>
+              <div class="cta57_heading-bottom">
+                <h2 class="h1 black _404-headline _404-m">are looking for does not exist</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section_faq9">
+    <div class="padding-global">
+      <div class="container-small">
+        <div class="padding-section-large-2">
+          <div class="margin-bottom margin-xxlarge">
+            <div class="max-width-large-2">
+              <div class="margin-bottom margin-small">
+                <h2 class="h2 center">Where next?</h2>
+              </div>
+            </div>
+          </div>
+          <div class="faq9_component">
+            <div class="kam-dal-row">
+              <div id="w-node-a1f52aaf-0716-bea3-7f5f-96aef242a893-99551718" class="kam-dal-title">
+                <div class="text">Do you want to stay?</div>
+              </div>
+              <a id="w-node-_222476bc-7cbe-0ac5-e034-a947fcfe2491-99551718" href="../en/accomodation" class="button-secondary v-90 w-button">View the apartments</a>
+            </div>
+            <div class="kam-dal-row">
+              <div id="w-node-a1f52aaf-0716-bea3-7f5f-96aef242a899-99551718" class="kam-dal-title">
+                <div class="text">Do you like delicious gastronomy?</div>
+              </div>
+              <a id="w-node-_1b65111b-f0ca-ce55-727d-fd31475d3092-99551718" href="../en/restaurant" class="button-secondary v-90 w-button">Into the restaurant</a>
+            </div>
+            <div class="kam-dal-row">
+              <div id="w-node-a1f52aaf-0716-bea3-7f5f-96aef242a89f-99551718" class="kam-dal-title">
+                <div class="text">Are you planning a private event?</div>
+              </div>
+              <a id="w-node-_1dd6db9a-d668-633d-6670-9993eb9caf36-99551718" href="../en/private-events" class="button-secondary v-90 w-button">What can we arrange</a>
+            </div>
+            <div class="kam-dal-row">
+              <div id="w-node-a1f52aaf-0716-bea3-7f5f-96aef242a8a5-99551718" class="kam-dal-title">
+                <div class="text">Do you have a question?</div>
+              </div>
+              <a id="w-node-db3faed6-6b8a-3c8f-08aa-b3538e591e21-99551718" href="../en/contact" class="button-secondary v-90 w-button">Get in touch</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="footer1_component-2">
+    <div class="gradient-up"></div>
+    <div class="padding-global">
+      <div class="container-large-3">
+        <div class="padding-vertical padding-xxlarge">
+          <div class="padding-bottom padding-xxlarge">
+            <div class="w-layout-grid footer1_top-wrapper-2">
+              <div class="footer1_left-wrapper">
+                <div class="margin-bottom margin-small flex-hor">
+                  <a href="../en/" class="footer1_logo-link w-nav-brand"><img src="../images/GAUDEO-LOGO-WHITE.svg" loading="lazy" alt="logo Gaudeo v bílé barvě" class="image footer-logo"></a>
+                  <div>
+                    <div class="margin-bottom margin-small">
+                      <div class="text-block-15">
+                        <a href="mailto:recepce@penzionygaudeo.cz" class="link small linkf-green">recepce@penzionygaudeo.cz</a>
+                      </div>
+                      <div class="text-block-14">
+                        <a href="tel:+420774869840" class="link small linkf-green">+420 774 869 840</a> <span class="white">(Mo-Su 9–16)</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <p class="text-footer">Accommodation at the Vranov reservoir that will appeal to all your senses. Come to enjoy your holiday with us.</p>
+              </div>
+              <div class="w-layout-grid footer1_menu-wrapper-2">
+                <div class="footer1_link-list-2">
+                  <a href="../en/accomodation" class="footer1_link">Accommodation</a>
+                  <a href="../en/reservation" class="footer1_link">Reservation</a>
+                  <a href="../en/restaurant" class="footer1_link">Restaurant</a>
+                </div>
+                <div class="footer1_link-list-2">
+                  <a href="../en/private-events" class="footer1_link">Private Events</a>
+                  <a href="../en/about" aria-current="page" class="footer1_link w--current">About</a>
+                  <a href="../en/contact" class="footer1_link">Contact</a>
+                </div>
+                <div class="footer1_link-list-2">
+                  <a href="../en/tips-for-trips" class="footer1_social-link w-inline-block">
+                    <div class="footer1_link pb-0-6">Tips for trips</div>
+                  </a>
+                  <a href="https://www.instagram.com/restaurace_gaudeo/" target="_blank" class="footer1_social-link w-inline-block">
+                    <div class="social-icon w-embed"><svg width="100%" height="100%" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M16 3H8C5.23858 3 3 5.23858 3 8V16C3 18.7614 5.23858 21 8 21H16C18.7614 21 21 18.7614 21 16V8C21 5.23858 18.7614 3 16 3ZM19.25 16C19.2445 17.7926 17.7926 19.2445 16 19.25H8C6.20735 19.2445 4.75549 17.7926 4.75 16V8C4.75549 6.20735 6.20735 4.75549 8 4.75H16C17.7926 4.75549 19.2445 6.20735 19.25 8V16ZM16.75 8.25C17.3023 8.25 17.75 7.80228 17.75 7.25C17.75 6.69772 17.3023 6.25 16.75 6.25C16.1977 6.25 15.75 6.69772 15.75 7.25C15.75 7.80228 16.1977 8.25 16.75 8.25ZM12 7.5C9.51472 7.5 7.5 9.51472 7.5 12C7.5 14.4853 9.51472 16.5 12 16.5C14.4853 16.5 16.5 14.4853 16.5 12C16.5027 10.8057 16.0294 9.65957 15.1849 8.81508C14.3404 7.97059 13.1943 7.49734 12 7.5ZM9.25 12C9.25 13.5188 10.4812 14.75 12 14.75C13.5188 14.75 14.75 13.5188 14.75 12C14.75 10.4812 13.5188 9.25 12 9.25C10.4812 9.25 9.25 10.4812 9.25 12Z" fill="CurrentColor"></path>
+                      </svg></div>
+                    <div class="link small white underline">Instagram</div>
+                  </a>
+                  <a href="https://www.facebook.com/restauraceGAUDEO/" target="_blank" class="footer1_social-link w-inline-block">
+                    <div class="social-icon w-embed"><svg width="100%" height="100%" viewbox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.5 6H13.5C12.9477 6 12.5 6.44772 12.5 7V10H16.5C16.6137 9.99748 16.7216 10.0504 16.7892 10.1419C16.8568 10.2334 16.8758 10.352 16.84 10.46L16.1 12.66C16.0318 12.8619 15.8431 12.9984 15.63 13H12.5V20.5C12.5 20.7761 12.2761 21 12 21H9.5C9.22386 21 9 20.7761 9 20.5V13H7.5C7.22386 13 7 12.7761 7 12.5V10.5C7 10.2239 7.22386 10 7.5 10H9V7C9 4.79086 10.7909 3 13 3H16.5C16.7761 3 17 3.22386 17 3.5V5.5C17 5.77614 16.7761 6 16.5 6Z" fill="CurrentColor"></path>
+                      </svg></div>
+                    <div class="link small white underline">Facebook</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="line-divider"></div>
+          <div class="padding-top padding-medium">
+            <div class="footer1_bottom-wrapper">
+              <div class="footer_gdpr_container">
+                <div class="footer1_credit-text">© 2023 Gaudeo</div>
+                <div class="w-layout-grid footer1_legal-list">
+                  <a href="../en/privacy-policy" class="link small mr-20 _0-8rem linkf-green">Privacy Policy</a>
+                  <a href="../en/terms-and-conditions" class="link small mr-20 _0-8rem linkf-green">Terms of conditions</a>
+                  <a href="../en/operating-rules" class="link small mr-20 _0-8rem linkf-green">Operating Rules</a>
+                  <a href="../en/cookies" class="link small _0-8rem linkf-green">Cookies</a>
+                </div>
+              </div>
+              <div class="w-layout-grid footer1_legal-list-payments">
+                <a href="#" class="payment-container mr-40px w-inline-block"><img src="../images/goPayicon-1.png" loading="lazy" id="w-node-_4547bd11-f846-6118-874c-3a766c49446b-09b5b970" alt="" class="image-9"></a>
+                <a href="#" class="payment-container mr-40px w-inline-block"><img src="../images/VISA.svg" loading="lazy" id="w-node-_8279d67b-9cc4-cb73-1f21-1e1c3ba6bcf0-09b5b970" alt="Logo aplikace VISA." class="image-9"></a>
+                <a href="#" class="w-inline-block"><img src="../images/MASTERCARD.svg" loading="lazy" id="w-node-dee4004b-e67e-4332-ac51-0f7a2f4df0c8-09b5b970" alt="Logo aplikace MasterCard." class="image-9"></a>
+                <div id="w-node-eb318634-42af-bed9-994f-f460fc0c3855-09b5b970" class="payment-container"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=63e4c1b68c122b237942add3" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="../js/webflow.js" type="text/javascript"></script>
+    <div id="_HotelczCertificate-Box" class="_left-side" data-hash="21808" data-seo="gaudeo">
+    <a href="https://gaudeo.hotel.cz/" id="_HC-HotelLink" title="Penziony a restaurace GAUDEO">Penziony a restaurace GAUDEO</a>
+    <a href="https://www.hotel.cz/stitary/" id="_HC-TownLink" title="Hotely a Ubytování Štítary">ve Štítarech</a>
+    <a href="https://gaudeo.hotel.cz/reviews/" id="_HC-ReviewLink">hodnocení</a>
+</div>
+<script type="text/javascript">
+    (function (d, s) {
+        var e = d.createElement(s); e.type = 'text/javascript'; e.async = true;
+        e.src = '//www.hotel.cz/hotelcz/scripts/external/certificate-2.0.min.js';
+        var t = d.getElementsByTagName(s)[0]; t.parentNode.insertBefore(e, t);
+    }(document, 'script'));
+</script>
+</body>
+</html>
